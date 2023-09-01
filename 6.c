fafff39f246ca0fe3b2b30130d315ccd16b53808
Josué Medino <josuemedino5@gmail.com>
2023-09-01 18:25:55 -0300

    another way answer

--- 6.c
+++ 6.c
@@ -1,105 +1,80 @@
+/*
+6. Escrever um programa para encontrar os coeficientes s e t da combinação linear
+mdc(a, b) = s · a + t · b.
+*/
+
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
 
 
-int euclides(int numberHigh, int numberLow){
-    //Verifica se o menor numero é zero
-    if (numberLow == 0){
-        return numberHigh;
-    }
-
-    //Caso resto zero, o divisor analisado é o MDC
-    if (numberHigh%numberLow == 0){
-        return numberLow;
-    } else {
-        return euclides(numberLow, numberHigh%numberLow);
-    }
-
-}
-
-
-
-
-void find(int a,int b, int mdc){
-   
-
-    int i = 0;
-    int j;
-    do
-    {
-        i++;
-        j = 0;
-
-        while ((a*i - b*j)>mdc)
-        {
-            j++;
-        }
-
-
-
-    } while ((a*i - b*j) != mdc);
-    
-
-
-    int k = 0;
-    int z;
-    do
-    {
-        k++;
-        z = 0;
-
-        while ((b*k - a*z)>mdc)
-        {
-            z++;
-        }
-
-
-
-    } while ((b*k - a*z) != mdc);
-
-    if(abs(i-j) < abs(k-z)){
-        printf("%d = (%d) * %d + (-%d) * %d\n\n", mdc, i, a, j,b);
-        printf("S = -%d\nT = %d\n", j, i);
-    } else {
-        printf("%d = (%d) * %d + (-%d) * %d\n", mdc, k, b, z,a);
-        printf("S = -%d\nT = %d\n", z, k);
-    }
-    
-    
-    
-}
-    
-
-
-
-
 int main(){
-    int numberOne, numberTwo, result;
 
-
-     scanf("%d %d", &numberOne, &numberTwo);
-
-    //verificando qual o maior numero
-    if(numberOne > numberTwo){
-        //Caso o primeiro seja o maior...
-        result = euclides(numberOne, numberTwo);
-        
-        
-    } else {
-        //caso o segundo seja o maior ...
-        result = euclides(numberTwo, numberOne);    
-        
+    printf("Digite os valores (a e b) separados por um espaço\n");
+    int a, b;
+    scanf("%d %d", &a, &b);
+    int A = a, B = b; 
+    //Reodernando de forma Decrescente 
+    if (b>a){
+        int aux = b;
+        b = a;
+        a = aux;
     }
-
-   
-
-
+    
+    //Definindo os Arrays Necessários
+    int quotient[100] = {}, values[100] = {};
     
 
+    //Determinando o mdc entre a e b
+    int i = 0;
+    int rest;
+    while (a%b != 0)
+    {   
+        quotient[i] = a/b;
+        rest = a%b;
+        a = b; 
+        b = rest;
+        i++;
+        printf("Valor de i é: %d\n", i);
+    }
+    
+  
+    if(b != 1){
+        printf("%d e %d não são primos entre si.\nPois possuem mdc igual a %d.\nLogo não possui solução!\n", A,B,b);
+    }
+    
+    int length = i ; 
     
     
-    find(numberOne,numberTwo, result);
+    for (int i = length - 1; i >= 0; i--)
+    {
+       if( i == length -1){
+            values[i] = quotient[i] * 1; 
+       } else if (i == length - 2){
+            values[i] = quotient[i]*values[i+1] + 1;
+       } else {
+            values[i] = quotient[i]*values[i+1] + values[i+2];
+       }
+    }
+    
+    printf(" %d", values[0]);
+    printf(" %d", values[1]);
+    printf(" %d", values[2]);
+
+
+    if(i % 2 == 0){
+        values[1] = -values[1];
+    } else {
+        values[0] = -values[0];
+    }
+    
+    printf("%d\n%d", values[length -2], values[length -1]);
+    printf("\nS = %d", values[length-3]);
+    printf("\nT = %d", values[1]);
+    
+
+
+
 
     return 0;
 }
