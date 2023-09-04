09f4b9f4c7d3e18804f57b40368d16259c6443bf
Josê <103127687+914joseph@users.noreply.github.com>
2023-09-03 21:24:16 -0300

    Update 10.c

--- 10.c
+++ 10.c
@@ -4,6 +4,35 @@ invés de somente 3, onde n será dado pelo usuário.
 */
 
 #include <stdio.h>
+
+// Função que calcula o MDC usando o Algoritmo de Euclides
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+// Função que verifica se dois números são coprimos (MDC igual a 1)
+int areCoprime(int a, int b) {
+    return gcd(a, b) == 1;
+}
+
+// Função que verifica se todos os valores de mArray são coprimos entre si
+int areAllCoprime(int mArray[], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (!areCoprime(mArray[i], mArray[j])) {
+                return 0; // Não são coprimos
+            }
+        }
+    }
+    return 1; // Todos são coprimos
+}
+
+// Função que calcula o inverso multiplicativo de 'a' mod 'm'
 int inverse(int a, int m){
     int i = 1;
 
@@ -32,7 +61,11 @@ int read(int n){
         M *= mArray[i];
     }
 
-    for (int i = 0; i < n; i++)
+    if(areAllCoprime(mArray,n)==0){
+        printf("os números digitados não são coprimos!\n");
+        return -3;
+    }else{
+        for (int i = 0; i < n; i++)
     {
         aArray[i] = M/mArray[i];
         sArray[i] = inverse(aArray[i], mArray[i]);
@@ -44,6 +77,9 @@ int read(int n){
     }
 
     return x%M;
+    }
+    
+    
     
     
    
@@ -55,6 +91,9 @@ int main(){
     scanf("%d", &n);
 
     int x = read(n);
+    if(x==-3){
+        return 0;
+    }
     printf("X = %d", x);
 
     return 0;
